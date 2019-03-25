45604cb07e31d14a6eb26499aefd7de2d8f45a0c
tunganh252 <tunganh2521999@gmail.com>
2019-03-25 23:33:55 +0700

    no message

--- src/controllers/index.js
+++
@@ -1,7 +0,0 @@
-import { NhanVien } from '../models/nhan-vien';
-import '../assets/css/style.css';
-import '../assets/scss/style.scss';
-
-let nhanVien = new NhanVien('Tung Anh', 20);
-console.log(nhanVien);
-
